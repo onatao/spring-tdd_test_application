c11656eb58cd778f7e22d9c79283e6ef08f55587
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-01 15:08:53 -0300

    removing unnecessary imports

--- src/test/java/dev/natao/tddtestapplication/controller/ProductControllerTest.java
+++ src/test/java/dev/natao/tddtestapplication/controller/ProductControllerTest.java
@@ -1,19 +1,15 @@
 package dev.natao.tddtestapplication.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import dev.natao.tddtestapplication.model.Product;
 
 /**
  * Mock - in this case, a dummy Product for
