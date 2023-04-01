e01a3464e826b60813553873acd03e056e25b390
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-01 12:56:20 -0300

    created CommissionConfiguration class

--- src/test/java/dev/natao/tddtestapplication/model/CommissionTest.java
+++ src/test/java/dev/natao/tddtestapplication/model/CommissionTest.java
@@ -2,10 +2,32 @@ package dev.natao.tddtestapplication.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootTest
 public class CommissionTest {
+
+    /**
+     *  This class will be executed before 
+     *  all the test methods.
+     */
+    @TestConfiguration
+    static class CommissionConfiguration {
+        /**
+         * This method will create a new Commission().
+         * @return a new instance of Commission.class 
+         */
+        @Bean
+        public Commission commission() {
+            return new Commission();
+        }
+    }
+    
+    @Autowired
+    Commission commissionTest;
 
      /**
      *  Calculate commission based on the value of
@@ -14,7 +36,6 @@ public class CommissionTest {
     @Test
     public void calculate_commission_upTo_1000() {
         // Arrange - preparation
-        Commission commissionTest = new Commission();
         Double standardValue = 1000d;
         Double commissionValue = 100d;
         // Act - Action
@@ -30,7 +51,6 @@ public class CommissionTest {
     @Test
     public void calculate_commission_above_2000() {
         // Arrange - preparation
-        Commission commissionTest = new Commission();
         Double standardValue = 2000d;
         Double commissionValue = 400d;
         // Act - Action
