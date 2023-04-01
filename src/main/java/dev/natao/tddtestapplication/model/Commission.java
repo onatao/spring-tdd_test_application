7514bd0ec9dd9adbb718148b322124405903fdec
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-01 12:48:11 -0300

    created CommissionTest methods

--- src/main/java/dev/natao/tddtestapplication/model/Commission.java
+++ src/main/java/dev/natao/tddtestapplication/model/Commission.java
@@ -3,7 +3,7 @@ package dev.natao.tddtestapplication.model;
 public class Commission {
 
     public Double commissionValue(Double amount) {
-        return (amount <= 1000d) ? amount * 0.10 : amount * 0.15;
+        return (amount <= 1000d) ? amount * 0.10 : amount * 0.20;
         /**
          * Uso do paràmetro ternário, se a condição for verdadeira (true), será
          * executado a operação após o " ? ", se for falso (false) é executado a 
--- src/test/java/dev/natao/tddtestapplication/model/CommissionTest.java
+++ src/test/java/dev/natao/tddtestapplication/model/CommissionTest.java
@@ -7,8 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class CommissionTest {
 
+     /**
+     *  Calculate commission based on the value of
+     *  0.10% for sales up to 1000d.
+     */
     @Test
-    public void calculate_commission_under_1000() {
+    public void calculate_commission_upTo_1000() {
         // Arrange - preparation
         Commission commissionTest = new Commission();
         Double standardValue = 1000d;
@@ -18,5 +22,22 @@ public class CommissionTest {
         // Assert - confirmation
         Assertions.assertEquals(commissionValue, finalValue);    
     }
-    
+
+    /**
+     *  Calculate commission based on the value of
+     *  0.20% for above to 2000.00 (included).
+     */
+    @Test
+    public void calculate_commission_above_2000() {
+        // Arrange - preparation
+        Commission commissionTest = new Commission();
+        Double standardValue = 2000d;
+        Double commissionValue = 400d;
+        // Act - Action
+        Double finalValue = commissionTest.commissionValue(standardValue);
+        // Assert - confirmation
+        Assertions.assertEquals(commissionValue, finalValue);
+          
+    }
+
 }
