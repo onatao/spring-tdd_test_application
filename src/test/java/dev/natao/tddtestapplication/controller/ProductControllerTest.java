076c59132ac156089cb5bc9649ae89969ca8227a
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-01 13:42:12 -0300

    first ProductControllerTest configuration

--- src/test/java/dev/natao/tddtestapplication/controller/ProductControllerTest.java
+++ src/test/java/dev/natao/tddtestapplication/controller/ProductControllerTest.java
@@ -1,5 +1,30 @@
 package dev.natao.tddtestapplication.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Mock - in this case, a dummy Product for
+ * testing purposes only
+ */
+@WebMvcTest // Testing an Web API 
 public class ProductControllerTest {
-    
+    // Mock MVC
+    // Mockito
+    // Mock   
+
+    @Autowired
+    private ProductController productController;
+
+    @Autowired
+    private MockMvc mockMvc; 
+
+    @BeforeEach 
+    public void setupMethod() {
+
+    }
+
+
 }
