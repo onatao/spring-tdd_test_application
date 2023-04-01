b11a9c576a77a8789aff5be1983c4d7d3c0beb14
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-01 13:23:30 -0300

    created test method for totalValue

--- src/test/java/dev/natao/tddtestapplication/model/ProductTest.java
+++ src/test/java/dev/natao/tddtestapplication/model/ProductTest.java
@@ -1,5 +1,7 @@
 package dev.natao.tddtestapplication.model;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +21,19 @@ public class ProductTest {
     @Autowired
     Product productTest;
 
-    
+    @Test
+    public void calculate_product_total_value() {
+        // Arrange - preparation
+        productTest.setProductQuantity(10);
+        productTest.setDiscountValue(10d);
+        productTest.setAdditionValue(0d);
+        productTest.setProductValue(10d);
+        Double expectedValue = 90d;
+        // Act - action
+        double result = productTest.totalValue();
+        // Assert - confirmation
+        Assertions.assertEquals(expectedValue, result);
+    }
 
 
     
