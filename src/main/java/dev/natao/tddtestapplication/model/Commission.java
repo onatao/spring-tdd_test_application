22118e20b188f3baa55922265ff872a0ce9ed226
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-01 12:30:46 -0300

    commissionValue method refactored

--- src/main/java/dev/natao/tddtestapplication/model/Commission.java
+++ src/main/java/dev/natao/tddtestapplication/model/Commission.java
@@ -3,7 +3,12 @@ package dev.natao.tddtestapplication.model;
 public class Commission {
 
     public Double commissionValue(Double amount) {
-        return 0d;
+        return (amount <= 1000d) ? amount * 0.10 : amount * 0.15;
+        /**
+         * Uso do paràmetro ternário, se a condição for verdadeira (true), será
+         * executado a operação após o " ? ", se for falso (false) é executado a 
+         * operação após o " : ".
+         */
     }
     
 }
