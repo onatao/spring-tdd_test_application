565b600dabf488589c625d80889ed66164fa81e2
Nathan Barros <nathanbarrosnt@gmail.com>
2023-04-03 20:34:05 -0300

    MockBean productService added

--- src/test/java/dev/natao/tddtestapplication/controller/ProductControllerTest.java
+++ src/test/java/dev/natao/tddtestapplication/controller/ProductControllerTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import dev.natao.tddtestapplication.service.ProductService;
 
 
 /**
@@ -25,8 +28,13 @@ public class ProductControllerTest {
     @Autowired
     private ProductController productController;
 
+    @MockBean
+    private ProductService productService;
+
     @Autowired
     private MockMvc mockMvc; 
+
+    private 
 
     @BeforeEach 
     public void setupMethod() {
